7d8fe85257dc9b5b6c64c786ee50281e541e78a7
x-color <36035885+x-color@users.noreply.github.com>
2018-08-03 02:39:05 +0900

    Change usage

--- jump.sh
+++ jump.sh
@@ -1,64 +1,62 @@
-# Jump to bookmarking directory
-
-# $ jump [-d TAG] [-l] [-h] [-t TAG] TAG
-# $ jump TAG    : go to directory of TAG
-# $ jump -d TAG : Delete TAG in tag list
-# $ jump -l     : List up tags
-# $ jump -h     : Show help
-# $ jump -t TAG : Add TAG to tag list
-
-function _jump_help {
-  echo -e "jump [-d TAG] [-l] [-h] [-t TAG] TAG\n"
-  echo "  jump TAG    : go to directory of TAG"
-  echo "  jump -d TAG : Delete TAG in tag list"
-  echo "  jump -l     : List up tags"
-  echo "  jump -h     : Show help"
-  echo "  jump -t TAG : Add TAG to tag list"
+function _usage_jump {
+  echo "Usage:"
+  echo "  jump [-d TAG] [-l] [-h] [-t TAG] TAG"
+  echo ""
+  echo "Description:"
+  echo "  Jump to tagging directory"
+  echo ""
+  echo "Options:"
+  echo "  -d    delete tag"
+  echo "  -l    list up tags"
+  echo "  -h    print help message"
+  echo "  -t    add tag for current directory"
 }
 
 function jump {
   local OPTIND OPTARG OPT
-  local FLG_ADD FLG_DEL FLG_LST TAG_ADD TAG_DEL
+  local _flg_add _flg_del _flg_lst _tag_add _tag_del
 
   # Options parser
   while getopts d:lht: OPT
   do
     case $OPT in
-      "d" ) FLG_DEL="TRUE" ; TAG_DEL="$OPTARG" ;;
-      "l" ) FLG_LST="TRUE" ;;
-      "h" ) _jump_help
+      "d" ) _flg_del="TRUE"
+            _tag_del="$OPTARG" ;;
+      "l" ) _flg_lst="TRUE" ;;
+      "h" ) _usage_jump
             return 1 ;;
-      "t" ) FLG_ADD="TRUE" ; TAG_ADD="$OPTARG" ;;
-        * ) echo "Usage: jump [-d TAG] [-l] [-h] [-t TAG] TAG" >&2
+      "t" ) _flg_add="TRUE"
+            _tag_add="$OPTARG" ;;
+        * ) _usage_jump
             return 1 ;;
     esac
   done
 
   # Delete bookmark
-  if [ "$FLG_DEL" = "TRUE" ]
+  if [ "$_flg_del" = "TRUE" ]
   then
     # Delete
-    sed -i -e '/^'$TAG_DEL',/d' ~/.jump_tags
+    sed -i -e '/^'$_tag_del',/d' ~/.jump_tags
   fi
 
   # Add tag
-  if [ "$FLG_ADD" = "TRUE" ]
+  if [ "$_flg_add" = "TRUE" ]
   then
     local DIC_PATH=`pwd`
-    local CHECK=`grep -e '^'$TAG_ADD',' ~/.jump_tags`
+    local CHECK=`grep -e '^'$_tag_add',' ~/.jump_tags`
     if [ "$CHECK" != "" ]
     then
       # Error : tag is already resistered in .jump_tags
-      echo -e "[$TAG_ADD] is already registered." >&2
+      echo -e "[$_tag_add] is already registered in tags." 1>&2
       return 1
     else
       # Add
-      echo "$TAG_ADD,$DIC_PATH" >> ~/.jump_tags
+      echo "$_tag_add,$DIC_PATH" >> ~/.jump_tags
     fi
   fi
 
   # List up tags
-  if [ "$FLG_LST" = "TRUE" ]
+  if [ "$_flg_lst" = "TRUE" ]
   then
     # List up
     cat ~/.jump_tags | sort | column -t -s,
@@ -69,18 +67,22 @@ function jump {
 
   # Check arguments
   if [ "$#" != "0" ]
-    then
+  then
     # Jump to tag
     local DIR_PATH=`grep -w -E "^$1" ~/.jump_tags | cut -d , -f2`
     if [ "$DIR_PATH" = "" ]
     then
       # Error : cannot find tag in .jump_tags
-      echo "[$1] can not find." >&2
+      echo "[$1] can not find in tags." 1>&2
       return 1
     else
       # Jump
       cd $DIR_PATH
     fi
+  elif [ "$OPTIND" = "1" ]
+  then
+    # No option and no argument
+    _usage_jump
   fi
 }
 
